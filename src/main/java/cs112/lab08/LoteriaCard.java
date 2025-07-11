af59f54cdafa97dd58be0fb50abe999f2193823c
Ramysannta <sannta.hyramy@gmail.com>
2025-07-11 08:48:26 +0000

    completed code

--- src/main/java/cs112/lab08/LoteriaCard.java
+++ src/main/java/cs112/lab08/LoteriaCard.java
@@ -1,136 +1,108 @@
+/**************************************************
+*  EChALE STEM Loteria - GUI JavaFX Program
+*  Author: Ramysannta Hy
+*  Description: Draws random STEM-themed Lotería cards
+*  Features: GUI layout, card drawing, event handling,
+*            prevents duplicates, progress bar, and end-game state
+**************************************************/
+
 package cs112.lab08;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.text.Font;
 import javafx.scene.image.Image;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
-/* UML CLASS DIAGRAM:
------------------------------------------
-LoteriaCard
------------------------------------------
-- cardName : String
-- imageName : String
-- cardNum : int
------------------------------------------
-+ LoteriaCard(cardName : String, imageName : String, cardNum : int)
-+ LoteriaCard(original : LoteriaCard)
-+ setAll(cardName : String, imageName : String, cardNum : int)
-+ getCardName() : String
-+ setCardName(cardName : String) : void
-+ getImageName() : String
-+ getImage() : Image
-+ setImageName(imageName : String) : void
-+ getCardNum() : int
-+ setCardNum(carNum : int) : void
-+ toString() : String
------------------------------------------
-*/
+import java.util.Random;
 
-public class LoteriaCard
-{
-    // INSTANCE VARIABLES
-    private String cardName = "";
-    private String imageName = "";
-    private int cardNum = 0;
+public class Main extends Application {
 
-    //Constructor
-    public LoteriaCard(String cardName, String imageName, int cardNum)
-    {
-        this.setAll(cardName, imageName, cardNum);
-    }
+    // GUI COMPONENTS
+    private Label titleLabel, messageLabel;
+    private ImageView cardImageView;
+    private Button drawCardButton;
+    private ProgressBar gameProgressBar;
 
-    //Copy constructor with error checking for null values
-    public LoteriaCard(LoteriaCard original)
-    {
-        if (original != null)
-        {
-            this.setAll(original.cardName, original.imageName, original.cardNum);
-        }
-        else
-        {
-            System.out.println("ERROR: trying to copy NULL object. Exiting program...");
-            System.exit(1);
-        }
-    }
+    // LOTERIA DATA
+    private LoteriaCard[] cards = {
+        new LoteriaCard("Ingeniera", "1.png", 1),
+        new LoteriaCard("Matemático", "2.png", 2),
+        new LoteriaCard("Científica", "3.png", 3),
+        new LoteriaCard("Astronauta", "4.png", 4)
+        // Add more cards if needed!
+    };
 
-    //default constructor creates false loteria card (like Joker cards in a 52 card standard deck)
-    public LoteriaCard() {
-        this("EChALE STEM Logo", "0.png", 0);
-    }
+    private boolean[] drawn = new boolean[cards.length];
+    private int drawnCount = 0;
+    private Random rand = new Random();
 
-    // get/set methods
-    public void setAll(String cardName, String imageName, int cardNum)
-    {
-        this.setCardName(cardName);
-        this.setImageName(imageName);
-        this.setCardNum(cardNum);
-    }
-
-    public String getCardName()
-    {
-        return this.cardName;
-    }
-
-    public void setCardName(String cardName)
-    {
-        this.cardName = cardName;
-    }
-
-    public String getImageName()
-    {
-        return this.imageName;
-    }
-
-	/**
-	* Gets Image object for this card by reading it from the appropriate location in a replit project
-	*
-	* @return returns Image object that can be used to display it in an ImageView in GUI
-	**/
-	public Image getImage() {
-		FileInputStream input = null;
-		try {
-			input = new FileInputStream("./src/main/resources/images/" + this.imageName);
-		} catch (FileNotFoundException e) {
-			//e.printStackTrace();
-			System.err.println("ERROR: could not open file.");
-			System.exit(0);
-		}
-		return new Image(input);
-	}
-
-    public void setImageName(String imageName)
-    {
-        this.imageName = imageName;
-    }
-
-    public int getCardNum()
-    {
-        return cardNum;
-    }
-
-    public void setCardNum(int cardNum)
-    {
-        this.cardNum = cardNum;
-    }
-
-    //toString method
     @Override
-    public String toString()
-    {
-        return "Loteria Card #" + this.cardNum + ": " + this.cardName + "(" + this.imageName + ")";
+    public void start(Stage primaryStage) {
+        // SETUP COMPONENTS
+        titleLabel = new Label("¡Lotería STEM!");
+        titleLabel.setFont(new Font("Arial", 28));
+        titleLabel.setTextFill(Color.DARKBLUE);
+
+        cardImageView = new ImageView();
+        cardImageView.setFitWidth(300);
+        cardImageView.setFitHeight(300);
+        cardImageView.setPreserveRatio(true);
+
+        messageLabel = new Label("Presiona el botón para sacar una carta...");
+        messageLabel.setFont(new Font(14));
+
+        drawCardButton = new Button("Sacar Carta");
+        gameProgressBar = new ProgressBar(0);
+
+        // EVENT HANDLING - Anonymous Class
+        drawCardButton.setOnAction(e -> {
+            if (drawnCount < cards.length) {
+                int index;
+                do {
+                    index = rand.nextInt(cards.length);
+                } while (drawn[index]);
+
+                drawn[index] = true;
+                drawnCount++;
+
+                LoteriaCard drawnCard = cards[index];
+                cardImageView.setImage(drawnCard.getImage());
+                messageLabel.setText("Sacaste: " + drawnCard.getCardName());
+
+                gameProgressBar.setProgress((double) drawnCount / cards.length);
+
+                if (drawnCount == cards.length) {
+                    // Game Over
+                    cardImageView.setImage(new LoteriaCard().getImage()); // Logo
+                    messageLabel.setText("GAME OVER. No more cards!\nExit and run again to reset ^_^");
+                    drawCardButton.setDisable(true);
+                    gameProgressBar.setStyle("-fx-accent: red;");
+                }
+            }
+        });
+
+        // SETUP LAYOUT
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(titleLabel, cardImageView, messageLabel, drawCardButton, gameProgressBar);
+
+        // SCENE + STAGE
+        Scene scene = new Scene(layout, 350, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("EChALE STEM Lotería");
+        primaryStage.show();
     }
 
-    //Equals method
-    @Override
-    public boolean equals(Object o)
-    {
-        if(o == null || !(o instanceof LoteriaCard))
-        {
-            return false;
-        }
-        LoteriaCard other = (LoteriaCard) o;
-
-        return this.cardName.equals(other.cardName) && this.cardNum == other.cardNum
-                && this.imageName.equals(other.imageName);
+    // MAIN DRIVER
+    public static void main(String[] args) {
+        launch(args);
     }
 }
